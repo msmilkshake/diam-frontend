fa8cc8219fdef57b1a2a799c40cea237cbd485a2
Tiag0L0b0 <113850501+Tiag0L0b0@users.noreply.github.com>
2024-05-04 13:13:31 +0100

    CartSidebar localStorage

--- src/components/CartSidebar.tsx
+++ src/components/CartSidebar.tsx
@@ -1,9 +1,43 @@
+import {Sidebar} from "primereact/sidebar";
+import {useContext, useState} from "react";
+import "primeflex/primeflex.css";
+import {Card} from "primereact/card";
+import {Button} from "primereact/button";
+import {CartContext, CartDispatchContext} from "../contexts/CartContext.ts";
 
 const CartSidebar = ({visible, setVisible}) => {
+    const cartItems = useContext(CartContext)
+    const cartDispatch = useContext(CartDispatchContext)
 
     return (
-        <></>
+        <>
+            <Sidebar position={"right"} visible={visible} onHide={() => setVisible(false)}>
+                <div>
+                    {cartItems!.length > 0 && cartItems!.map(item => <CartItem id={item.id} qty={item.amount}></CartItem>)}
+                    {cartItems?.length === 0 && <span>empty cart</span>}
+                </div>
+            </Sidebar>
+        </>
     );
 };
 
 export default CartSidebar;
+
+const CartItem = ({id, qty}) => {
+    return (
+        <>
+           <Card>
+               <div className="flex flex-column gap-2">
+                   <span>id: {id}</span>
+                   <span>amount: {qty}</span>
+                   <div>
+                       <Button>-</Button>
+                       <Button>+</Button>
+                       <Button>Delete</Button>
+                   </div>
+
+               </div>
+           </Card>
+        </>
+    );
+};
--- src/contexts/CartContext.ts
+++ src/contexts/CartContext.ts
@@ -15,33 +15,40 @@ export const CartDispatchContext = createContext<Dispatch<CartAction> | null>(
   null,
 );
 
-export const cartReducer = (cartItems: CartItem[], action: CartAction) => {
+export const cartReducer = (cartItems: CartItem[], action: CartAction): CartItem[] => {
   const filteredCartItems = cartItems.filter(
     (item) => item.id !== (action.payload as CartItem).id,
   );
+  let results: CartItem[];
   switch (action.type) {
-    case "add": {
-      return [...filteredCartItems, action.payload];
-    }
+    case "add":
+      results = [...filteredCartItems, action.payload as CartItem];
+      break;
 
-    case "update": {
-      return cartItems.map((item) =>
+
+    case "update":
+      results = cartItems.map((item) =>
         item.id === (action.payload as CartItem).id
           ? { ...item, amount: (action.payload as CartItem).amount }
           : item,
       );
-    }
+      break;
 
     case "remove":
-      return filteredCartItems;
+      results = filteredCartItems;
+      break;
 
     case "clear":
-      return [];
+      results = [];
+      break;
 
     case "restore":
-      return action.payload as CartItem[];
+      results = action.payload as CartItem[];
+      break;
 
     default:
       return cartItems;
   }
+  localStorage.setItem("anonymous-cart", JSON.stringify(results))
+  return results
 };
