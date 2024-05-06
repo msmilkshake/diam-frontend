fe32f03617e1ea477db0ed7b595f23a560b7db39
Switchy <miguelbarros3000@gmail.com>
2024-05-06 23:08:11 +0100

    Changes to home page carousel

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Carousel, CarouselResponsiveOption } from "primereact/carousel";
 import { Button } from "primereact/button";
 import React, { useEffect, useState } from "react";
-import { ProductProps } from "./ProductCard.tsx";
+import ProductCard, { ProductProps } from "./ProductCard.tsx";
 import ApiService, { BASE_URL } from "../services/ApiService.ts";
 import { Link } from "react-router-dom";
 import { Card } from "primereact/card";
@@ -21,23 +21,28 @@ const HomePage = () => {
   }, []);
 
   const responsiveOptions: CarouselResponsiveOption[] = [
+      {
+          breakpoint: "2500px",
+          numVisible: 4,
+          numScroll: 1,
+      },
+      {
+          breakpoint: "1900px",
+          numVisible: 3,
+          numScroll: 1,
+      },
     {
       breakpoint: "1400px",
-      numVisible: 2,
-      numScroll: 1,
-    },
-    {
-      breakpoint: "1199px",
       numVisible: 3,
       numScroll: 1,
     },
     {
-      breakpoint: "767px",
+      breakpoint: "1199px",
       numVisible: 2,
       numScroll: 1,
     },
     {
-      breakpoint: "575px",
+      breakpoint: "800px",
       numVisible: 1,
       numScroll: 1,
     },
@@ -45,121 +50,76 @@ const HomePage = () => {
 
   const productTemplate = (product: ProductProps) => {
     return (
-      // <>
-      //   <div className="border-1 surface-border border-round m-2 text-center py-5 px-3 h-27rem">
-      //     <Link to={`/products/${product.id}`}>
-      //       <Card>
-      //         <div
-      //           className="flex flex-column align-items-center gap-2"
-      //         >
-      //           <div className="relative">
-      //             {product.discountPercent && (
-      //               <div className={`absolute ${styles.floatDiscount} p-3`}>
-      //                 {product.discountPercent} %
-      //               </div>
-      //             )}
-      //             <img
-      //               className="w-full h-full shadow-2 h-15rem"
-      //               src={`${BASE_URL}/${product.imageUrl}`}
-      //               alt=""
-      //             />
-      //           </div>
-      //           <span className={`${styles.productName} productName`}>
-      //             {product.name}
-      //           </span>
-      //           <div className="flex flex-row justify-content-between align-items-end w-full">
-      //             <div>
-      //               {product.inStock && (
-      //                 <span className={`${styles.inStock}`}>
-      //                   <i className="bi bi-check-circle-fill"></i>
-      //                   {" Em stock"}
-      //                 </span>
-      //               )}
-      //               {!product.inStock && (
-      //                 <span className={`${styles.outOfStock}`}>
-      //                   <i className="bi bi-x-circle-fill"></i>
-      //                   {" Sem stock"}
-      //                 </span>
-      //               )}
-      //             </div>
-      //             <div
-      //               className={`flex flex-column justify-content-end align-items-end ${styles.priceContainer}`}
-      //             >
-      //               {product.discountPrice && (
-      //                 <span className={`${styles.originalPrice}`}>
-      //                   {product.price.toFixed(2)} €
-      //                 </span>
-      //               )}
-      //               <span className={`${styles.price}`}>
-      //                 Preço:{" "}
-      //                 <span className={`${styles.price} ${styles.val}`}>
-      //                   {product.discountPrice
-      //                     ? product.discountPrice.toFixed(2)
-      //                     : product.price.toFixed(2)}{" "}
-      //                   €
-      //                 </span>
-      //               </span>
-      //             </div>
-      //           </div>
-      //         </div>
-      //       </Card>
-      //     </Link>
-      //   </div>
-      // </>
-      <Link to={`/products/${product.id}`}>
-        <div className="border-1 surface-border border-round m-2 w-27rem text-center py-3 px-3">
-          <div className="mb-3">
-            <img
-              src={`${BASE_URL}/${product.imageUrl}`}
-              alt={product.name}
-              className="w-7 shadow-2"
-            />
-          </div>
-
-          <div className="mt-1 mb-3">
-            {product.name}
-          </div>
-          <div className="flex flex-row justify-content-between align-items-end w-full">
-            <div>
-              {product.inStock && (
-                <span className={`${styles.inStock}`}>
-                  <i className="bi bi-check-circle-fill"></i>
-                  {" Em stock"}
+      <>
+        <div className="productCard p-0 m-5 w-20 mt-3">
+          <Link to={`/products/${product.id}`}>
+            <Card>
+              <div
+                className={`flex flex-column align-items-center gap-2 ${styles.cardContent}`}
+              >
+                <div className="w-fit relative">
+                  {product.discountPercent && (
+                    <div className={`absolute ${styles.floatDiscount} p-3`}>
+                      {product.discountPercent} %
+                    </div>
+                  )}
+                  <img
+                    className={`${styles.cardImage}`}
+                    src={`${BASE_URL}/${product.imageUrl}`}
+                    alt=""
+                  />
+                </div>
+                <span className={`${styles.productName} productName`}>
+                  {product.name}
                 </span>
-              )}
-              {!product.inStock && (
-                <span className={`${styles.outOfStock}`}>
-                  <i className="bi bi-x-circle-fill"></i>
-                  {" Sem stock"}
+                <span className={`${styles.overflowDescription} text-justify`}>
+                  {product.description}
                 </span>
-              )}
-            </div>
-            <div
-              className={`flex flex-column justify-content-end align-items-end ${styles.priceContainer}`}
-            >
-              {product.discountPrice && (
-                <span className={`${styles.originalPrice}`}>
-                  {product.price.toFixed(2)} €
-                </span>
-              )}
-              <span className={`${styles.price}`}>
-                Preço:{" "}
-                <span className={`${styles.price} ${styles.val}`}>
-                  {product.discountPrice
-                    ? product.discountPrice.toFixed(2)
-                    : product.price.toFixed(2)}{" "}
-                  €
-                </span>
-              </span>
-            </div>
-          </div>
+                <div className="flex flex-row justify-content-between align-items-end w-full">
+                  <div>
+                    {product.inStock && (
+                      <span className={`${styles.inStock}`}>
+                        <i className="bi bi-check-circle-fill"></i>
+                        {" Em stock"}
+                      </span>
+                    )}
+                    {!product.inStock && (
+                      <span className={`${styles.outOfStock}`}>
+                        <i className="bi bi-x-circle-fill"></i>
+                        {" Sem stock"}
+                      </span>
+                    )}
+                  </div>
+                  <div
+                    className={`flex flex-column justify-content-end align-items-end ${styles.priceContainer}`}
+                  >
+                    {product.discountPrice && (
+                      <span className={`${styles.originalPrice}`}>
+                        {product.price.toFixed(2)} €
+                      </span>
+                    )}
+                    <span className={`${styles.price}`}>
+                      Preço:{" "}
+                      <span className={`${styles.price} ${styles.val}`}>
+                        {product.discountPrice
+                          ? product.discountPrice.toFixed(2)
+                          : product.price.toFixed(2)}{" "}
+                        €
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </Card>
+          </Link>
         </div>
-      </Link>
+      </>
     );
   };
 
   return (
     <>
+        <h1 className="mb-0 pb-0">Produtos com desconto</h1>
       <Carousel
         verticalViewPortHeight="150px"
         value={products}
@@ -167,6 +127,7 @@ const HomePage = () => {
         numScroll={3}
         responsiveOptions={responsiveOptions}
         itemTemplate={productTemplate}
+        circular
       />
       HomePage works! :)
     </>
